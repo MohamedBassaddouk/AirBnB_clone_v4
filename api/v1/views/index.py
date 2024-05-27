8dc5bbdf75468f66efa2809f25cbb97ad74d4e16
“abdullah <abdullah.h.elgabry@gmail.com>
2024-05-27 19:27:59 +0300

    v3 airbnb

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
-"""main file"""
+""" This is the main index.py file """
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models.amenity import Amenity
-
-classes = {
-    "Amenity": Amenity,
-    "City": City,
-    "Place": Place,
-    "Review": Review,
-    "State": State,
-    "User": User,
-}
 
 
-@app_views.route("/status")
-def get_status():
-    """return route stat"""
-    return jsonify({"status": "OK"})
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    # setting up the resp
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
 
 
-@app_views.route("/stats")
-def get_stats():
-    """notify"""
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
 
-    return jsonify(
-        {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User),
-        }
-    )
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
